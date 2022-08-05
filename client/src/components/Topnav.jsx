c46f2d163f7619e8e80ef18ef5edb0baa0af0a1b
listerineh <jsalvarez4@espe.edu.ec>
2022-08-05 18:26:45 -0500

    Fix: topnav layout for responsive

--- client/src/components/Topnav.jsx
+++ client/src/components/Topnav.jsx
@@ -2,14 +2,14 @@ import { MoonIcon, SunIcon } from "@heroicons/react/outline";
 
 const Topnav = ({ userId, theme, handleThemeSwitch }) => {
   return (
-    <div className="h-1/6 bg-indigo-800 dark:bg-indigo-900">
-      <div className="flow-root p-5">
-        <div className="float-left pt-3">User: {userId}</div>
+    <div className="h-1/6 bg-indigo-800 dark:bg-indigo-900 flex text-center items-center justify-center">
+      <div className="flow-root p-5 w-full">
+        <div className="float-left">User: {userId}</div>
         <div className="float-right">
           <button
             type="button"
             onClick={handleThemeSwitch}
-            className="p-2 md:mt-2 text-white text-lg hover:animate-pulse"
+            className="text-white text-lg hover:animate-pulse"
           >
             {theme === "light" ? (
               <SunIcon
