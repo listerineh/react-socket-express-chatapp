7e7cb485bb38c5677a22da0c77746d8bf3d294aa
listerineh <jsalvarez4@espe.edu.ec>
2022-09-20 17:45:01 -0500

    feat: add toggle button to show the users info, dark mode and logout button

--- client/src/components/Topnav.jsx
+++ client/src/components/Topnav.jsx
@@ -4,6 +4,10 @@ import { MoonIcon, SunIcon, UserIcon } from "@heroicons/react/outline";
 const Topnav = ({ userId, theme, handleThemeSwitch }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleOpenDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="h-1/6 bg-indigo-800 dark:bg-indigo-900 flex text-center items-center justify-center">
       <div className="flow-root p-5 w-full">
@@ -13,20 +17,43 @@ const Topnav = ({ userId, theme, handleThemeSwitch }) => {
         <div className="float-right">
           <div className="flex space-x-3">
             <button
-              type="button"
-              onClick={handleThemeSwitch}
-              className={`text-white text-lg p-2 ${
-                theme === "light"
-                  ? "hover:text-yellow-300 hover:border-yellow-300"
-                  : "hover:text-gray-400 hover:border-gray-400"
-              }`}
+              onClick={handleOpenDropdown}
+              className="text-white text-lg p-2 "
             >
-              {theme === "light" ? (
-                <SunIcon className="block h-6 w-6" aria-hidden="true" />
-              ) : (
-                <MoonIcon className="block h-6 w-6" aria-hidden="true" />
-              )}
+              <UserIcon className="block h-6 w-6" aria-hidden="true" />
             </button>
+            <div
+              className={`${
+                !isOpen ? "hidden" : "absolute right-3 top-20"
+              } w-44 font-normal rounded divide-y divide-gray-100 shadow bg-indigo-500 `}
+            >
+              <ul className="py-1 text-md font-thin text-white">
+                <li className="block py-2 px-4 hover:bg-indigo-400">
+                  Logged as: <span className="uppercase">{userId}</span>
+                </li>
+                <li className="block py-2 px-4 hover:bg-indigo-400">
+                  <button
+                    type="button"
+                    onClick={handleThemeSwitch}
+                    className="text-white text-lg p-2"
+                  >
+                    <div className="flex font-thin text-md">
+                      <p>
+                        Mode: <span className="uppercase">{theme}</span>
+                      </p>
+                    </div>
+                  </button>
+                </li>
+              </ul>
+              <div className="py-1">
+                <a
+                  href="#"
+                  className="block py-2 px-4 hover:bg-red-400 text-white uppercase"
+                >
+                  Sign out
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
